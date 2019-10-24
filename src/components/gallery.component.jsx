27743b0500d7b61cb41aa94702b7ad01295716e4
Gustavo Carrillo (Laptop) <g.yokebed@gmail.com>
2019-10-23 23:09:39 -0500

    Added infinite scroll

--- src/components/gallery.component.jsx
+++ src/components/gallery.component.jsx
@@ -1,27 +1,49 @@
 import React, { Component } from "react";
 import axios from "axios";
+import InfiniteScroll from 'react-infinite-scroller';
 
 const apiEndpoint = "http://sinembargo.test/wp-json/wp/v2/instagram-gallery";
 
 class Gallery extends Component {
   state = {
-    posts: []
+    posts: [],
+    page: 1,
+    hasmore: true
   };
 
-  async componentDidMount() {
-    const { data: posts } = await axios.get(apiEndpoint);
-    this.setState({ posts });
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const { posts, page } = this.state;
+    const { data: newPosts, headers } = await axios.get(`${apiEndpoint}?page=${page}&per_page=8`);
+
+    this.setState({
+      posts: [...posts, ...newPosts],
+      page: page + 1,
+      hasmore: page < headers["x-wp-totalpages"]
+    });
   }
 
   render() {
-    return this.state.posts.map(post => (
-      <a href={post.ACF.link_instagram} key={post.id}>
-        <img
-          alt={post.ACF.imagen_instagram.title}
-          src={post.ACF.imagen_instagram.url}
-        />
-      </a>
-    ));
+    return (
+      <InfiniteScroll
+        loadMore={this.fetchData.bind(this)}
+        hasMore={this.state.hasmore}
+        useWindow={true}
+        loader={<div className="loader" key={0}>Cargando ...</div>}
+      >
+        {this.state.posts.map(post => (
+          <a href={post.ACF.link_instagram} key={post.id}>
+            <img
+              alt={post.ACF.imagen_instagram.title}
+              src={post.ACF.imagen_instagram.url}
+            />
+          </a>))
+        }
+      </InfiniteScroll>
+    )
   }
 }
 
