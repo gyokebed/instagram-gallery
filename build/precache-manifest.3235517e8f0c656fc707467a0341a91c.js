55fd366d90efef89faa475ca99eadb6f966cbdeb
Gustavo Carrillo (Laptop) <g.yokebed@gmail.com>
2019-10-25 13:12:11 -0500

    Removed build folder from .gitignore file

---
+++ build/precache-manifest.3235517e8f0c656fc707467a0341a91c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb83764e3bf784e089eefd185ed4b45",
+    "url": "/react/instagram-gallery/build/index.html"
+  },
+  {
+    "revision": "31452b4b93852e55db34",
+    "url": "/react/instagram-gallery/build/static/css/main.dbdcdd36.chunk.css"
+  },
+  {
+    "revision": "0af7d01181f17bee0658",
+    "url": "/react/instagram-gallery/build/static/js/2.5be624e6.chunk.js"
+  },
+  {
+    "revision": "31452b4b93852e55db34",
+    "url": "/react/instagram-gallery/build/static/js/main.b8eacabf.chunk.js"
+  },
+  {
+    "revision": "d16fa6904decaaf7e776",
+    "url": "/react/instagram-gallery/build/static/js/runtime-main.94c58792.js"
+  }
+]);
